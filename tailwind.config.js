ea620c01a8b5f58d052423f9463dcc6b9a135b8f
LucasCarvaho <lucasstc@gmail.com>
2025-04-11 15:28:53 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-module.exports = { content: ["./index.html", "./src/**/*.{js,jsx}"], theme: { extend: {} }, plugins: [] };
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#00C2CB",
+          dark: "#1C3F60"
+        }
+      },
+    },
+  },
+  plugins: [],
+};
